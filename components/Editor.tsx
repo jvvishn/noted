d8dc09668f1a60303895b479cb501ec5c9e47512
jvvishn <jvvishn@gmail.com>
2026-05-22 11:29:05 -0500

    Added persisting editor content to localStorage

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,14 +1,22 @@
-"use client"
+"use client";
 
 import "@blocknote/core/fonts/inter.css";
-import "@blocknote/mantine/style.css"
-import { useCreateBlockNote } from "@blocknote/react";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote, useEditorChange } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 
 export default function Editor() {
-    const editor = useCreateBlockNote({});
-    
-    return (
-        <BlockNoteView editor={editor} />
-    );
+  const saved = typeof window !== "undefined" ? localStorage.getItem("noteData") : null;
+  const initialData = saved ? JSON.parse(saved) : undefined;
+
+  const editor = useCreateBlockNote({
+    initialContent: initialData,
+  });
+
+  useEditorChange((editor) => {
+    localStorage.setItem("noteData", JSON.stringify(editor.document));
+    console.log("saved: ", editor.document);
+  }, editor);
+
+  return <BlockNoteView editor={editor} theme="dark" />;
 }
